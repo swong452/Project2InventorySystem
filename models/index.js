84bbf74417bbccf067eacd447fa061a248f7a727
samson wong <steelerwong2005@hotmail.com>
2020-08-23 15:53:16 -0400

    add console log to index.js

--- models/index.js
+++ models/index.js
@@ -7,12 +7,21 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
 
+console.log('------');
+console.log('ENV: ', env);
+console.log('CONFIG: ', config);
+console.log('------');
+
 const basename = path.basename(module.filename);
 const db = {};
 let sequelize;
 
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable]);
+  console.log('------');
+  console.log('CONFIG.USE_ENV_VARIABLE: ', config.use_env_variable);
+  console.log('JAWSDB_URL: ', process.env.JAWSDB_URL);
+  console.log('------');
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
